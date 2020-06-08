148a2ff3d13467b4640c288dd2214cd54cb17422
marciovieira1 <marcio.vieira1@gmail.com>
2020-06-08 01:32:05 -0300

    Seeding service

--- SalesWebMVC/Migrations/20200607235636_OtherEntities.Designer.cs
+++ SalesWebMVC/Migrations/20200607235636_OtherEntities.Designer.cs
@@ -48,7 +48,7 @@ namespace SalesWebMVC.Migrations
 
                     b.HasIndex("SellerId");
 
-                    b.ToTable("SalesRecords");
+                    b.ToTable("SalesRecord");
                 });
 
             modelBuilder.Entity("SalesWebMVC.Models.Seller", b =>
