1abc2dc1f8f85f0f327e4333fc52cb443e32ae18
marciovieira1 <marcio.vieira1@gmail.com>
2020-06-10 15:07:06 -0300

    Validation

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -8,17 +8,29 @@ namespace SalesWebMVC.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="O campo {0} é obrigatório")]
+        [StringLength(60,MinimumLength =3,ErrorMessage ="O {0} deve ter entre {2} e {1} caracteres")]
         [Display(Name="Nome")]
         public string Name { get; set; }
+
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage ="Email informado é inválido")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [Display(Name="Data de Nascimento")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString ="{0:dd/MMM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
         [Display(Name ="Salário Base")]
         [DisplayFormat(DataFormatString ="{0:c}")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} deve ser maior ou igual a {1} e menor ou igual a {2}")]
         public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
         [Display(Name ="Departamento")]
         public int DepartmentId { get; set; }
